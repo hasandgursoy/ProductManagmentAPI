e2d6e3111922e0cf0f91f7c34f13ddbb189ec237
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-05-25 23:55:08 +0300

    Product Controller is started

---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Entities;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    
+    public class ProductsController : CustomBaseController // CustomBaseController'ı miras alıyor.
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IMapper mapper, IService<Product> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+        
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+
+            return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+
+
+
+        }
+
+    }
+}
